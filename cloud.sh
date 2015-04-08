2a601a3c1579b0e8207b2be001179ae52582712d
Clinton Sprain <clintonsprain@gmail.com>
2015-04-07 19:59:07 -0500

    Agnostify directory references

--- cloud.sh
+++ cloud.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
-cd /sys/devices/platform/cros_ec_lpc.0/cros-ec-dev.0/chromeos/cros_ec/lightbar
-echo 0xFF > brightness
-echo stop > sequence
-for i in 1 to 30
+
+count=3
+speed=0.1
+path=$(sh init.sh)
+
+for i in `seq 1 $count`
 do
-	echo "0 0 0 255  1 0 0 255  2 255 255 0  3 0 0 255" > led_rgb
-	sleep 2
-	echo "0 0 0 255  1 0 0 255  2 255 255 0  3 175 175 255" > led_rgb
-	sleep 2
-	echo "0 0 0 255  1 0 0 255  2 255 255 0  3 255 255 255" > led_rgb
-	sleep 2
-	echo "0 100 100 150  1 100 100 150  2 255 255 125  3 175 175 255" > led_rgb
-	sleep 2
-	echo "0 125 125 100  1 125 125 100  2 255 255 200  3 125 125 100" > led_rgb
-	sleep 4
-	echo "0 100 100 150  1 175 175 255  2 255 255 125  3 100 100 150" > led_rgb
-	sleep 2
-	echo "0 0 0 255  1 255 255 255  2 255 255 0  3 0 0 255" > led_rgb
-	sleep 2
-	echo "0 175 175 255  1 175 175 255  2 255 255 0  3 0 0 255" > led_rgb
-	sleep 2
-	echo "0 255 255 255  1 0 0 255  2 255 255 0  3 0 0 255" > led_rgb
-	sleep 2
-	echo "0 175 175 255  1 0 0 255  2 255 255 0  3 0 0 255" > led_rgb
-	sleep 2
-	echo "0 0 0 255  1 0 0 255  2 255 255 0  3 0 0 255" > led_rgb
-	sleep 2
+	echo "0 0 0 255  1 0 0 255  2 255 255 0  3 0 0 255" > "$path/led_rgb"
+	sleep $speed
+	echo "0 0 0 255  1 0 0 255  2 255 255 0  3 175 175 255" > "$path/led_rgb"
+	sleep $speed
+	echo "0 0 0 255  1 0 0 255  2 255 255 0  3 255 255 255" > "$path/led_rgb"
+	sleep $speed
+	echo "0 100 100 150  1 100 100 150  2 255 255 125  3 175 175 255" > "$path/led_rgb"
+	sleep $speed
+	echo "0 125 125 100  1 125 125 100  2 200 200 150  3 125 125 100" > "$path/led_rgb"
+	sleep $speed
+	sleep $speed
+	echo "0 100 100 150  1 175 175 255  2 255 255 125  3 100 100 150" > "$path/led_rgb"
+	sleep $speed
+	echo "0 0 0 255  1 255 255 255  2 255 255 0  3 0 0 255"  > "$path/led_rgb"
+	sleep $speed
+	echo "0 175 175 255  1 175 175 255  2 255 255 0  3 0 0 255" > "$path/led_rgb"
+	sleep $speed
+	echo "0 255 255 255  1 0 0 255  2 255 255 0  3 0 0 255" > "$path/led_rgb"
+	sleep $speed
+	echo "0 175 175 255  1 0 0 255  2 255 255 0  3 0 0 255" > "$path/led_rgb"
+	sleep $speed
+	echo "0 0 0 255  1 0 0 255  2 255 255 0  3 0 0 255" > "$path/led_rgb"
+	sleep $speed
 done
-cd ~
-sh lightbar.sh
+sh reset-lightbar.sh $path
--- knightrider.sh
+++ knightrider.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
-cd /sys/devices/platform/cros_ec_lpc.0/cros-ec-dev.0/chromeos/cros_ec/lightbar
-echo 0xFF > brightness
-echo stop > sequence
-for i in 1 2 3 4 5
+
+count=3
+speed=0.1
+path=$(sh init.sh)
+
+for i in `seq 1 $count`
 do
-	echo "0 255 0 0  1 0 0 0  2 0 0 0  3 0 0 0" > led_rgb
+	echo "0 255 0 0  1 0 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 255 0 0  2 0 0 0  3 0 0 0" > led_rgb
+	echo "0 0 0 0  1 255 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 0 0 0  2 255 0 0  3 0 0 0" > led_rgb
+	echo "0 0 0 0  1 0 0 0  2 255 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 0 0 0  2 0 0 0  3 255 0 0" > led_rgb
+	echo "0 0 0 0  1 0 0 0  2 0 0 0  3 255 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 0 0 0  2 255 0 0  3 0 0 0" > led_rgb
+	echo "0 0 0 0  1 0 0 0  2 255 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 255 0 0  2 0 0 0  3 0 0 0" > led_rgb
+	echo "0 0 0 0  1 255 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 125 0 0  1 0 0 0  2 0 0 0  3 0 0 0" > led_rgb
+	echo "0 125 0 0  1 0 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 125 0 0  2 0 0 0  3 0 0 0" > led_rgb
+	echo "0 0 0 0  1 125 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 0 0 0  2 125 0 0  3 0 0 0" > led_rgb
+	echo "0 0 0 0  1 0 0 0  2 125 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 0 0 0  2 0 0 0  3 125 0 0" > led_rgb
+	echo "0 0 0 0  1 0 0 0  2 0 0 0  3 125 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 0 0 0  2 125 0 0  3 0 0 0" > led_rgb
+	echo "0 0 0 0  1 0 0 0  2 125 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
-	echo "0 0 0 0  1 125 0 0  2 0 0 0  3 0 0 0" > led_rgb
+	echo "0 0 0 0  1 125 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
 	sleep .1
 done
-cd ~
-sh reset-lightbar.sh
+sh reset-lightbar.sh $path
