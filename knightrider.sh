67a1d93204e2157f73752a3bd862766b78640f52
Clinton Sprain <clintonsprain@gmail.com>
2015-04-07 20:57:30 -0500

    Speed

--- knightrider.sh
+++ knightrider.sh
@@ -7,28 +7,28 @@ path=$(sh init.sh)
 for i in `seq 1 $count`
 do
 	echo "0 255 0 0  1 0 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 255 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 0 0 0  2 255 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 0 0 0  2 0 0 0  3 255 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 0 0 0  2 255 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 255 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 125 0 0  1 0 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 125 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 0 0 0  2 125 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 0 0 0  2 0 0 0  3 125 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 0 0 0  2 125 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 	echo "0 0 0 0  1 125 0 0  2 0 0 0  3 0 0 0" > "$path/led_rgb"
-	sleep .1
+	sleep $speed
 done
 sh reset-lightbar.sh $path
